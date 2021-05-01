cbaeb9ebb76ca7c6f3aa8595ff2514772789944f
search-ish <73951031+search-ish@users.noreply.github.com>
2021-05-01 16:10:43 -0400

    Update proapi.py

--- proapi.py
+++ proapi.py
@@ -1,12 +1,12 @@
 import json
 import requests
-PROPUBLICA_API_KEY = "hSgyc8zlknHEQtBBNflRDt9obUHIKEqaUqi8yei"
+PROPUBLICA_API_KEY = "PUT YOUR API KEY HERE BETWEEN THE PARANS"
 
 headers = {"X-API-Key": PROPUBLICA_API_KEY}
 
-
-api_url = ("https://api.propublica.org/congress/v1/116}/bills/hr195/statements.json")
-
+api_url = ("https://api.propublica.org/congress/v1/116}/bills/hr195/statements.json") 
+#THE URL ABOVE IS A DEFAULT URL SEARCHING FOR STATEMENTS ON A BILL NAMED HR195
+#GO TO PROPUBLICA API DOCUMENTATION TO FIND HOW TO MAKE YOUR OWN URL
 
 api_response = requests.get(api_url, headers=headers)
 
