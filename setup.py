e9f7f88a9f8c73b2a978c99c44485eefae59bd7e
search-ish <73951031+search-ish@users.noreply.github.com>
2021-05-01 16:08:15 -0400

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='propublica',
-    install_requires=["requests"],
-    classifiers=[
-        "Programming Language :: Python",
-    ],
-)
