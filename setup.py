e32d3dfa968ab94b5469616094e9e45f1d68e1c5
search-ish <73951031+search-ish@users.noreply.github.com>
2021-05-01 15:54:29 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='propublica',
+    install_requires=["json", "requests"],
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+)
